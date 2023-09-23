230500e474225d83e7370547301f257227bdeb75
Ilya <i.anzhin@g.nsu.ru>
2023-09-23 21:37:29 +0700

    task-1 doc

--- task-1/BitArray.h
+++ task-1/BitArray.h
@@ -23,64 +23,92 @@ private:
     //used amount of bits
     int currSizeInBites;
 
-    //return index of last unused byte
+    //returns index of last unused byte
     [[nodiscard]] int lastUnusedByteInd() const;
 public:
 
+    //creates empty array reserving memory with size of one 'blockType'
     BitArray();
 
+    //clears allocated memory
     ~BitArray();
 
+    //creates array with size of numbits, first size of 'blockType' bytes initialises as 'value'
     explicit BitArray(int numBits, blockType value = 0);
 
+    //copy constructor
     BitArray(const BitArray &b);
 
+    //swaps two arrays, throw runtime error if sizes are not equal
     void swap(BitArray &other);
 
+    //copies other's array bits, resizes array if needed
     BitArray& operator=(const BitArray& other);
 
+    //changes size of array to numbits, if new size bigger, then new bits initialises as 'value'
     void resize(int numBits, bool value = false);
 
+    //changes size of array to 0
     void clear();
 
+    //adds new bit to the end
     void pushBack(bool bit);
 
+    //Bitwise AND, changes original array, throw runtime error if sizes are not equal
     BitArray& operator&=(const BitArray& b);
 
+    //Bitwise OR, changes original array, throw runtime error if sizes are not equal
     BitArray& operator|=(const BitArray& b);
 
+    //Bitwise XOR, changes original array, throw runtime error if sizes are not equal
     BitArray& operator^=(const BitArray& b);
 
+    //Bitwise left shift, changes original array
     BitArray& operator<<=(int n);
 
+    //Bitwise right shift, changes original array
     BitArray& operator>>=(int n);
 
+    //Bitwise left shift, create new object
     BitArray operator<<(int n) const;
 
+    //Bitwise right shift, changes create new object
     BitArray operator>>(int n) const;
 
+    //sets bit at index n to the value = 'val', throw range error if index out of range
     BitArray& set(int n, bool val = true);
 
+    //sets all bits to the 1
     BitArray<blockType>& set();
 
+    //sets bit at index n to the value = 0, throw range error if index out of range
     BitArray& reset(int n);
 
+    //sets all bits to the 0
     BitArray& reset();
 
+    //returns true if there is any nonzero bit
     bool any() const;
 
+    //returns true if all bits are zero
     bool none() const;
 
+    //Bitwise NOT, creates new object
     BitArray operator~() const;
 
+    //returns amount of nonzero bits
     int count() const;
 
+    //returns value of bit at 'i' index, throw range error if index out of range
     bool operator[](int i) const;
 
+    //returns amount of bits, used in array
     int size() const;
 
+    //returns true if size is 0
     bool empty() const;
 
+    //turn every byte to char, returns string of this chars
     std::string to_string() const;
 };
 
